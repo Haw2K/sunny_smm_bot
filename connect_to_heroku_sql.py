caf8422c03cb0ce96c2774ff2c9a4fe06779f0f5
haw2k <haw22k@gmail.com>
2018-05-02 12:27:21 -0700

    add insta api

---
+++ connect_to_heroku_sql.py
@@ -0,0 +1,28 @@
+import psycopg2
+
+# Connect to an existing database
+conn = psycopg2.connect("postgres://ilwxnlakhzsygl:1bb3b4c1078941d7c9cc0ec9a2df9e32c3989e3347149a259fdbd571ed51871b@ec2-107-21-103-146.compute-1.amazonaws.com:5432/demh6l5vnhecot")
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+# Execute a command: this creates a new table
+# cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+#
+# # Pass data to fill a query placeholders and let Psycopg perform
+# # the correct conversion (no more SQL injections!)
+# >>> cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",
+# ...      (100, "abc'def"))
+#
+# # Query the database and obtain data as Python objects
+fff = cur.execute("SELECT * FROM telegram_users")
+fdfd = cur.fetchone()
+fdfdf = cur.fetchall()
+
+
+# Make the changes to the database persistent
+#>>> conn.commit()
+
+# Close communication with the database
+cur.close()
+conn.close()
--- telegram_bot.py
+++ telegram_bot.py
@@ -4,7 +4,7 @@ import telebot
 from telebot import types
 from flask import Flask, request, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
-
+from srcApp import instagram_api
 
 TOKEN = '535439906:AAH3ZL2Yr64_lNnYEZlEepsPZXQoJyfr1S8'
 bot = telebot.TeleBot(TOKEN)
@@ -22,6 +22,7 @@ class telegram_users(db.Model):
     last_name = db.Column(db.String(100))
     username = db.Column(db.String(100))
     language_code = db.Column(db.String(100))
+    telegram_users_insta_accounts = db.relationship('telegram_users_insta_accounts', backref='telegram_users_insta_accounts', lazy=True)
 
     def __init__(self, id, is_bot=False, first_name='test', last_name='test', username='test', language_code='test'):
         self.id = id
@@ -34,25 +35,74 @@ class telegram_users(db.Model):
     def __repr__(self):
         return '<telegram_users %r>' % self.id
 
+class telegram_users_insta_accounts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    telegram_id = db.Column(db.Integer, db.ForeignKey('telegram_users.id'), nullable=False)
+    login = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    need_confirm_ip = db.Column(db.Bollean)
+
+    def __init__(self, id, telegram_id, login, password):
+        self.id = id
+        self.telegram_id = telegram_id
+        self.login = login
+        self.password = password
+
+
+    def __repr__(self):
+        return '<telegram_users %r>' % self.id
+
 db.create_all()
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    text = 'Greetings! Im Sunny SMM Robot! Send` /add ` to create new task. Want to know about all my options? Send` /help `and a list of the commands available for you will show up.`'
+    text = 'Greetings! Im Sunny SMM Robot! Send` /add ` to create new task. Want to know about all my options?' \
+           ' Send` /help `and a list of the commands available for you will show up.`'
     user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
     user_markup.row('Site', 'FAQ')
     bot.send_message(message.from_user.id, text, reply_markup=user_markup)
 
 @bot.message_handler(commands=['add'])
 def add(message):
+    markup = types.ReplyKeyboardRemove(selective=False)
+
     telegram_user = telegram_users.query.filter_by(id = message.from_user.id).first()
     if telegram_user == None:
         telegram_user = telegram_users(message.from_user.id)
         db.session.add(telegram_user)
         db.session.commit()
-        bot.send_message(message.from_user.id, telegram_users.query.all()[1].id)
+        #bot.send_message(message.from_user.id, telegram_users.query.all()[1].id, reply_markup=markup)
     else:
-        bot.send_message(message.from_user.id, 'you allready have account: %s' % (telegram_user.id))
+        #bot.send_message(message.from_user.id, 'you allready have account: %s' % (telegram_user.id), reply_markup=markup)
+
+    #add inline buttons create new accounts
+    keyboard = types.InlineKeyboardMarkup()
+    callback_button = types.InlineKeyboardButton(text="Add new instagram account", callback_data="add_new")
+    keyboard.add(callback_button)
+
+    #seach allready exist instagram accounts
+    insta_accounts = telegram_users_insta_accounts.query.filter_by(telegram_id = message.from_user.id).all()
+    if insta_accounts == None:
+        for insta_account in insta_accounts:
+            callback_button = types.InlineKeyboardButton(text='Edit account: %s' % (insta_account.login), callback_data=insta_account.id)
+            keyboard.add(callback_button)
+
+    bot.send_message(message.from_user.id, 'Select menu item:', reply_markup=keyboard)
+
+    #'when we need confim we send you email'
+    #instagram_api.get_total_followers_direct_login("nurtdinov.danil", 'Mitra123', 'd0394ffe96:09de558d36@194.28.194.111:52593')
+
+# В большинстве случаев целесообразно разбить этот хэндлер на несколько маленьких
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    # Если сообщение из чата с ботом
+    if call.message:
+        if call.data == "add_new":
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="chat")
+    # Если сообщение из инлайн-режима
+    elif call.inline_message_id:
+        if call.data == "test":
+            bot.edit_message_text(inline_message_id=call.inline_message_id, text="inline"
 
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
